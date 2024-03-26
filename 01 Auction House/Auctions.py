1f3557219739e472d3b274d227ee0221c33f0f00
Srikanth Haritsya <srikanth.haritsya@gmail.com>
2024-03-26 22:58:04 +0530

    V2

--- 01 Auction House/Auctions.py
+++ 01 Auction House/Auctions.py
@@ -5,7 +5,7 @@ def welcome():
     print(logo) 
     print('Welcome to the Auction\n\n')
 
-record = {}
+log = {}
 auction = True
 while auction:
     
@@ -14,14 +14,15 @@ while auction:
     bidder_name = input('Enter your name: ')      
     bidder_bid = int(input('Enter your bid: ₹ '))
     
-    record[bidder_name] = bidder_bid
+    log[bidder_name] = bidder_bid
     
     print(f'\nThank you {bidder_name}, you have bid ₹ {bidder_bid}')
     
+
     next_person = input('Are there other bidders? (y/n)\n').lower()
     
     if next_person != 'y':
         
         auction = False # This is the condition to stop the loop.
         
-        print(f'\nThe winner is {max(record, key=record.get)} with a bid of ₹ {max(record.values())}\n\n')
+        print(f'\nThe winner is {max(log, key=log.get)} with a bid of ₹ {max(log.values())}\n\n')
